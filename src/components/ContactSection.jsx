20f2307c454d05c7518efad6d2da243dde20186b
ronizera <roniltonbreno@gmail.com>
2026-02-04 11:47:25 -0300

    fix bug

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -26,7 +26,7 @@ export const ContactSection = () => {
 
           {/* LinkedIn Button */}
           <a
-            href="www.linkedin.com/in/ronilton-breno"
+            href="https://www.linkedin.com/in/ronilton-breno"
             target="_blank"
             rel="noopener noreferrer"
             className={cn("cosmic-button flex items-center gap-2 px-6 py-3")}
